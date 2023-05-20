53066bf1cd46b654b948acb9b1ee271d5e7e4e02
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-20 12:52:34 -0600

    Correctly tracking coordinates

--- cnc/cnc.ino
+++ cnc/cnc.ino
@@ -1,43 +1,43 @@
-#define motor1_stepPin 2
-#define motor1_dirPin 3
+#define motor1StepPin 2
+#define motor1DirPin 3
 
-#define motor2_stepPin 4
-#define motor2_dirPin 5
-
-const int stepsPerRevolution = 200;
+#define motor2StepPin 4
+#define motor2DirPint 5
 
 int currentX = 0;
 int currentY = 0;
 
 void setup() {
-    pinMode(motor1_stepPin, OUTPUT);
-    pinMode(motor1_dirPin, OUTPUT);
-    pinMode(motor2_stepPin, OUTPUT);
-    pinMode(motor2_dirPin, OUTPUT);
+    Serial.begin(9600);
+
+    pinMode(motor1StepPin, OUTPUT);
+    pinMode(motor1DirPin, OUTPUT);
+    pinMode(motor2StepPin, OUTPUT);
+    pinMode(motor2DirPint, OUTPUT);
 }
 
 void loop() {
-    int targetX = readXCoordinate();
-    int targetY = readYCoordinate();
+    if (Serial.available() <= 0)
+        return;
+
+    String input = Serial.readStringUntil('\n');
+    int xIndex = input.indexOf('X');
+    int yIndex = input.indexOf('Y');
+
+    String xSubstring = input.substring(xIndex + 1, yIndex);
+    String ySubstring = input.substring(yIndex + 1, input.length());
+
+    int targetX = xSubstring.toInt();
+    int targetY = ySubstring.toInt();
+
     int deltaX = targetX - currentX;
     int deltaY = targetY - currentY;
 
-    int totalSteps = max(abs(deltaX), abs(deltaY));
+    moveStepper(motor1StepPin, motor1DirPin, deltaX);
+    moveStepper(motor2StepPin, motor2DirPint, deltaY);
 
-    float xStepSize = deltaX / (float)totalSteps;
-    float yStepSize = deltaY / (float)totalSteps;
-
-    for (int i = 0; i < totalSteps; i++) {
-        int xSteps = round(currentX + i * xStepSize);
-        int ySteps = round(currentY + i * yStepSize);
-
-        moveStepper(motor1_stepPin, motor1_dirPin, xSteps);
-
-        moveStepper(motor2_stepPin, motor2_dirPin, ySteps);
-
-        currentX = xSteps;
-        currentY = ySteps;
-    }
+    currentX = targetX;
+    currentY = targetY;
 }
 
 void moveStepper(int stepPin, int dirPin, int steps) {
@@ -50,18 +50,8 @@ void moveStepper(int stepPin, int dirPin, int steps) {
 
     for (int i = 0; i < steps; i++) {
         digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);
+        delay(100);
         digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
+        delay(100);
     }
-}
-
-int readXCoordinate() {
-    int xCoordinate = analogRead(A0);
-    return xCoordinate;
-}
-
-int readYCoordinate() {
-    int yCoordinate = analogRead(A1);
-    return yCoordinate;
 }
