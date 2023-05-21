c95c1f0e74c943a307a3f482d4d0c071a2caebe6
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-20 18:18:56 -0600

    Latest arduino code

--- cnc/cnc.ino
+++ cnc/cnc.ino
@@ -1,40 +1,144 @@
-#define motor1StepPin 2
-#define motor1DirPin 3
+#define motor1_stepPin 2
+#define motor1_dirPin 3
 
-#define motor2StepPin 4
-#define motor2DirPint 5
+#define motor2_stepPin 4
+#define motor2_dirPin 5
+
+#define motor3_stepPin 6
+#define motor3_dirPin 7
 
 int currentX = 0;
 int currentY = 0;
+int currentZ = 0;
+int instruction = 0;
+char *token;
+const char s[2] = " ";
+char *x;
+char *y;
+int count = 0;
 
 void setup() {
     Serial.begin(9600);
 
-    pinMode(motor1StepPin, OUTPUT);
-    pinMode(motor1DirPin, OUTPUT);
-    pinMode(motor2StepPin, OUTPUT);
-    pinMode(motor2DirPint, OUTPUT);
+    pinMode(motor1_stepPin, OUTPUT);
+    pinMode(motor1_dirPin, OUTPUT);
+    pinMode(motor2_stepPin, OUTPUT);
+    pinMode(motor2_dirPin, OUTPUT);
+    pinMode(motor3_stepPin, OUTPUT);
+    pinMode(motor3_dirPin, OUTPUT);
 }
 
 void loop() {
+
     if (Serial.available() <= 0)
         return;
 
     String input = Serial.readStringUntil('\n');
-    int xIndex = input.indexOf('X');
-    int yIndex = input.indexOf('Y');
 
-    String xSubstring = input.substring(xIndex + 1, yIndex);
-    String ySubstring = input.substring(yIndex + 1, input.length());
+    // char* pinput = input.toCharArray();
 
-    int targetX = xSubstring.toInt();
-    int targetY = ySubstring.toInt();
+    if (input == "TOOL_UP") {
+        instruction = 0;
+    } else if (input == "TOOL_DOWN") {
+        instruction = 1;
+    } else if (strstr(input.c_str(), "MOVE_TO")) {
+        instruction = 2;
+    } else if (strstr(input.c_str(), "CHANGE_COLOR")) {
+        instruction = 3;
+    }
+
+    switch (instruction) {
+
+    case 0: // TOOL_UP
+        toolUp();
+        break;
+
+    case 1: // TOOL_DOWN
+        toolDown();
+        break;
+
+    case 2: // MOVE_TO
+        count = 0;
+        token = strtok(input.c_str(), s);
+        while (token != NULL) {
+            token = strtok(NULL, s);
+            count++;
+            if (count == 1) {
+                x = token;
+            } else if (count == 2) {
+                y = token;
+            }
+        }
+        moveMotor();
+        break;
+
+    case 3: // CHANGE_COLOR
+        if (strstr(input.c_str(), "RED")) {
+            Serial.println("rojo");
+            returnHome();
+        } else if (strstr(input.c_str(), "GREEN")) {
+            Serial.println("verde");
+        } else if (strstr(input.c_str(), "BLUE")) {
+            Serial.println("azul");
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+void returnHome() {
+
+    toolUp();
+
+    x = 0;
+    y = 0;
+
+    moveMotor();
+
+    if (Serial.available() <= 0)
+        return;
+}
+
+void changeColor() {}
+
+void toolDown() {
+
+    digitalWrite(motor3_dirPin, HIGH);
+
+    for (int i = 0; i < 950; i++) {
+        digitalWrite(motor3_stepPin, HIGH);
+        delay(1);
+        digitalWrite(motor3_stepPin, LOW);
+        delay(1);
+    }
+}
+
+void toolUp() {
+
+    digitalWrite(motor3_dirPin, LOW);
+
+    for (int i = 0; i < 950; i++) {
+        digitalWrite(motor3_stepPin, HIGH);
+        delay(1);
+        digitalWrite(motor3_stepPin, LOW);
+        delay(1);
+    }
+}
+
+void moveMotor() {
+    int targetX = atoi(x);
+    int targetY = atoi(y);
 
     int deltaX = targetX - currentX;
     int deltaY = targetY - currentY;
 
-    moveStepper(motor1StepPin, motor1DirPin, deltaX);
-    moveStepper(motor2StepPin, motor2DirPint, deltaY);
+    moveStepper(motor1_stepPin, motor1_dirPin, deltaX);
+    moveStepper(motor2_stepPin, motor2_dirPin, deltaY);
+
+    Serial.println(deltaX);
+    Serial.println(deltaY);
 
     currentX = targetX;
     currentY = targetY;
@@ -50,8 +154,8 @@ void moveStepper(int stepPin, int dirPin, int steps) {
 
     for (int i = 0; i < steps; i++) {
         digitalWrite(stepPin, HIGH);
-        delay(100);
+        delay(1);
         digitalWrite(stepPin, LOW);
-        delay(100);
+        delay(1);
     }
 }
