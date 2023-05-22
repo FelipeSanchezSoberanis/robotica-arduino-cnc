ad204bb4b0f2f322b042fede20c3e1d48b4b514b
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-22 13:24:47 -0600

    Working serial example

--- serialTesting/serialTesting.ino
+++ serialTesting/serialTesting.ino
@@ -3,6 +3,35 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+void parseCoords(String input, int* x, int* y) {
+  String coordsString = input.substring(input.indexOf(" ") + 1, input.length());
+  int spaceIndex = coordsString.indexOf(" ");
+  *x = coordsString.substring(0, spaceIndex).toInt();
+  *y = coordsString.substring(spaceIndex + 1, coordsString.length()).toInt();
+}
+void parseColor(String input, String* color) {
+  *color = input.substring(input.indexOf(" ") + 1, input.length());
+}
+
+void toolUp() { Serial.println("Received command TOOL_UP"); }
+void toolDown() { Serial.println("Received command TOOL_DOWN"); }
+void moveTo(String input) {
+  Serial.println("Received command MOVE_TO");
+  int x, y;
+  parseCoords(input, &x, &y);
+  Serial.print("x: ");
+  Serial.println(x);
+  Serial.print("y: ");
+  Serial.println(y);
+}
+void changeColor(String input) {
+  Serial.println("Received command CHANGE_COLOR");
+  String color;
+  parseColor(input, &color);
+  Serial.print("Color: ");
+  Serial.println(color);
+}
+
 void loop() {
   if (Serial.available() <= 0) return;
 
@@ -10,13 +39,15 @@ void loop() {
   input.trim();
   input.toUpperCase();
 
-  if (input == "ON") {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else if (input == "OFF") {
-    digitalWrite(LED_BUILTIN, LOW);
+  if (input == "TOOL_UP") {
+    toolUp();
+  } else if (input == "TOOL_DOWN") {
+    toolDown();
+  } else if (input.startsWith("MOVE_TO")) {
+    moveTo(input);
+  } else if (input.startsWith("CHANGE_COLOR")) {
+    changeColor(input);
   }
-
-  delay(100);
 
   Serial.println("DONE");
 }
